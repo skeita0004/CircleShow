6b7ba1a6a2c9b4d41074a50ba9d9c50ef2e4912e
啓大 齋藤 <240004@jc-21.jp>
2025-12-12 14:28:58 +0900

    main関数作った DxLib追加、設定もそれに伴ったものにした

---
+++ CircleShow/theMain.cpp
@@ -0,0 +1,13 @@
+#include <DxLib.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int APIENTRY WinMain(_In_     HINSTANCE hInstance,
+					 _In_opt_ HINSTANCE hPrevInstance,
+					 _In_     LPSTR     lpCmdLine,
+					 _In_     INT       nCmdShow)
+{
+
+}
