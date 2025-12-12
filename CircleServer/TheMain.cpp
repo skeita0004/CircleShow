aca6033bc57c271b0e875d8d5f7057f24efc00e2
3doriTea <sorasora3program@gmail.com>
2025-12-12 15:27:47 +0900

    バッファサイズの追加

--- CircleServer/TheMain.cpp
+++ CircleServer/TheMain.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "WSAIncluding.h"
 #include "Server.h"
+#include "BufferSize.h"
 
 
 int main()
 {
-	
+	std::cout << BUFFER_SIZE << std::endl;
 
 	std::cout << "サーバー予定" "やってこー" << std::endl;
 	return 0;
