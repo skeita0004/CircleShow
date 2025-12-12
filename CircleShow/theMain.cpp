361e5635e1ed46664ca0411a48a7db5907de617e
240145 <240145@E0403-05>
2025-12-12 15:07:54 +0900

    DxLibの設定をした

--- CircleShow/theMain.cpp
+++ CircleShow/theMain.cpp
@@ -1,8 +1,12 @@
-#include <DxLib.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+#include <DxLib.h>
 
 #pragma comment(lib, "ws2_32.lib")
+
+//ウィンドウサイズ
+const int WIN_WIDTH = 800;
+const int WIN_HEIGHT = 600;
 
 struct Circle
 {
@@ -19,5 +23,40 @@ int APIENTRY WinMain(_In_     HINSTANCE hInstance,
 					 _In_     LPSTR     lpCmdLine,
 					 _In_     INT       nCmdShow)
 {
+    //DxLibの初期化・ウィンドウ処理
+    ChangeWindowMode(true);
+    SetWindowSizeChangeEnableFlag(false, false);
+    SetMainWindowText("Client");
+    SetGraphMode(WIN_WIDTH, WIN_HEIGHT, 32);
+    SetBackgroundColor(0, 0, 0);
+    SetAlwaysRunFlag(1);
 
+    if (DxLib_Init() == -1)
+    {
+        return -1;
+    }
+
+    SetDrawScreen(DX_SCREEN_BACK);
+
+    while (true)
+    {
+        ClearDrawScreen();
+
+        //動作確認用の円
+        DrawCircle(WIN_WIDTH / 2, WIN_HEIGHT / 2, 10, GetColor(255, 0, 0));
+
+        ScreenFlip();
+        WaitTimer(16);
+        if (ProcessMessage() == -1)
+        {
+            break;
+        }
+        if (CheckHitKey(KEY_INPUT_ESCAPE))
+        {
+            break;
+        }
+    }
+
+    DxLib_End();
+    return 0;
 }
