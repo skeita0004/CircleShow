085710ea5799e0b19f334f6b0e91c9752e3145b5
240145 <240145@E0403-05>
2025-12-12 14:27:13 +0900

    コメントの追加

--- CircleShow/theMain.cpp
+++ CircleShow/theMain.cpp
@@ -4,8 +4,8 @@
 
 struct Circle
 {
-	void Load(char* p);
-	void Store(char* p);
+	void Load(char* p);//ネットワークから受信したデータを変換して受け取る
+	void Store(char* p);//ネットワークに送信するデータを変換して送る
 	int color;
 	int x;
 	int y;
